c0e0ef4b8d5e6a0215f2ee3a070e5e64383c0cd1
Riley Mione <rmione33@gmail.com>
2020-04-09 00:15:25 -0400

    Very minor changes to serial stuff.

--- Plotter.py
+++ Plotter.py
@@ -1,18 +1,31 @@
 import matplotlib
-import pyserial
+import serial
+import struct
+
+
 BAUD_RATE = 9600
 
 # todo: This may end up looking like a script, or may have functions.
 
 
 # Grabs serial output from the Esduino.
-esduino_output = pyserial.serial(port='COM3', baudrate=BAUD_RATE, bytesize=8, parity=pyserial.PARITY_NONE)
-esduino_output.open()
+esduino_output = serial.Serial(port='COM3', baudrate=BAUD_RATE, bytesize=8, parity=serial.PARITY_NONE)
+#esduino_output.open()
 
 
-if esduino_output.is_open() == True:
-    print("Initiating output")
-    # Now here, we will have some sort of logic to find out what mode we are in. Then mode 1 or 0 are called accordingly
+
+print("Initiating output")
+# Now here, we will have some sort of logic to find out what mode we are in. Then mode 1 or 0 are called accordingly
+while(True):
+    x = esduino_output.read(4)
+    print(x)
+    pair = struct.unpack('<hh', x)
+    print(pair)
+    # print()
+    # pair = struct.unpack('<cc', x)
+    # number  = int(pair[0])*10 + int(pair[1])
+    # print(number)
+
 else: print("\nError in opening serial port. Please try again.")
 
 def mode0_plotting():
